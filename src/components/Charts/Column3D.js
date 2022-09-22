e1e1e9d76e0a267f1cdda41a87b1488c27d74ffe
ceeing27 <112378177+ceeing27@users.noreply.github.com>
2022-09-22 22:05:23 +0800

    修改项目

---
+++ src/components/Charts/Column3D.js
@@ -0,0 +1,48 @@
+// STEP 1 - Include Dependencies
+// Include react
+import React from "react";
+
+// Include the react-fusioncharts component
+import ReactFC from "react-fusioncharts";
+
+// Include the fusioncharts library
+import FusionCharts from "fusioncharts";
+
+// Include the chart type
+import Column2D from "fusioncharts/fusioncharts.charts";
+
+// Include the theme as fusion
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+
+// Adding the chart and theme as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
+
+// STEP 2 - Chart Data
+
+// STEP 3 - Creating the JSON object to satore the chart configurations
+
+const Column3d = ({ data }) => {
+  const chartConfigs = {
+    type: "column3d", // The chart type
+    width: "100%", // Width of the chart
+    height: "400", // Height of the chart
+    dataFormat: "json", // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        caption: "Most popular",
+        yAxisName: "Stars",
+        xAxisName: "Repos",
+        xAxisNameFontSize: "16px",
+        yAxisNameFontSize: "16px",
+        theme: "fusion",
+        palettecolors: "5d62b5,29c3be,f2726f",
+      },
+      // Chart Data
+      data,
+    },
+  };
+  return <ReactFC {...chartConfigs} />;
+};
+
+export default Column3d;
